136101bc744a37b3449829dd86396e0612efd3ba
Jekson <jeksonshar@gmail.com>
2025-04-22 22:49:49 +0300

    implemented basic navigation component example with CompositionLocal for using navController in children

---
+++ app/src/main/java/com/example/navigationcomponenttest/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.navigationcomponenttest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationcomponenttest.ui.screens.AddItemRoute
+import com.example.navigationcomponenttest.ui.screens.ItemRoute
+import com.example.navigationcomponenttest.ui.screens.LocalNavController
+import com.example.navigationcomponenttest.ui.screens.add.AddItemScreen
+import com.example.navigationcomponenttest.ui.screens.items.ItemsScreen
+import com.example.navigationcomponenttest.ui.theme.NavigationComponentTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationComponentTestTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NavApp() {
+    val navController = rememberNavController()
+    CompositionLocalProvider(
+        LocalNavController provides navController
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = ItemRoute,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            composable(ItemRoute) { ItemsScreen() }
+            composable(AddItemRoute) { AddItemScreen() }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun NavAppPreview() {
+    NavigationComponentTestTheme {
+        NavApp()
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponenttest/ui/screens/AppRoutes.kt
@@ -0,0 +1,4 @@
+package com.example.navigationcomponenttest.ui.screens
+
+const val ItemRoute = "items"
+const val AddItemRoute = "add-item"
